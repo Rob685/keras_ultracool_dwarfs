c292a40937096f05f6076c5a29776239d6ee8e6f
Rob685 <rtejada@stanford.edu>
2019-07-07 16:50:08 -0700

    added cosine

--- calc.py
+++ calc.py
@@ -4,12 +4,15 @@ import matplotlib.pyplot as plt
 
 x = np.arange(0,2.0*np.pi,0.001)
 y = np.sin(x)
+z = np.cos(x)
 
-df = pd.DataFrame({'angle':x,'sine(x)':y})
+df = pd.DataFrame({'angle':x,'sine(x)':y,'cos(x)':z})
 
 print(df)
 
-plt.plot(df['angle'],df['sine(x)'],'k--')
+plt.plot(df['angle'],df['sine(x)'],'b--',label='sin(x)')
+plt.plot(df['angle'],df['cos(x)'],'r--',label='cos(x)')
 plt.xlabel('angle')
-plt.ylabel('sine(x)')
+plt.ylabel('trig_func')
+plt.legend()
 plt.show()
