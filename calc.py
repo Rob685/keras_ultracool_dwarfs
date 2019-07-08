b0d1db4d027a4ca9f9fdf0d467fedb9242823719
Rob685 <rtejada@stanford.edu>
2019-07-07 17:57:05 -0700

    slight change

--- calc.py
+++ calc.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-"""This is a test file for Atom. -Rob"""
+"""This is a test file for Atom. - Rob"""
 
 x = np.arange(0, 2.0*np.pi, 0.001)
 y = np.sin(x)
