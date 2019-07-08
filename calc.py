096a64c65f3ccc9d49dc78b66911e32204696c19
Rob685 <rtejada@stanford.edu>
2019-07-07 17:45:18 -0700

    Created a message

--- calc.py
+++ calc.py
@@ -2,24 +2,26 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-x = np.arange(0,2.0*np.pi,0.001)
+"""This is a test file for Atom. -Rob"""
+
+x = np.arange(0, 2.0*np.pi, 0.001)
 y = np.sin(x)
 z = np.cos(x)
 w = np.log(np.sin(x))
 
-df = pd.DataFrame({'angle':x,'sine(x)':y,'cos(x)':z,'log(x)':w})
+df = pd.DataFrame({'angle': x, 'sine(x)': y, 'cos(x)': z, 'log(x)': w})
 
 print(df)
 
-plt.plot(df['angle'],df['sine(x)'],'b--',label='sin(x)')
-plt.plot(df['angle'],df['cos(x)'],'r--',label='cos(x)')
-#plt.plot(df['angle'],df['log(x)'])
+plt.plot(df['angle'], df['sine(x)'], 'b--', label='sin(x)')
+plt.plot(df['angle'], df['cos(x)'], 'r--', label='cos(x)')
+# plt.plot(df['angle'],df['log(x)'])
 plt.xlabel('angle')
 plt.ylabel('output')
 plt.legend()
 plt.show()
 
-plt.plot(df['angle'],df['log(x)'],'k--',label='log(x)')
+plt.plot(df['angle'], df['log(x)'], 'k--', label='log(x)')
 plt.xlabel('angle')
 plt.ylabel('output')
 plt.legend()
